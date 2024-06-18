7b27adf5b5b1e97975f3d96e9c404bafe93f4028
max.zuendorf <jazuendorf@stud.mediadesign.de>
2024-06-18 17:01:57 +0200

    look at Item

--- PFITS_Unity/Assets/Scripts/UI/Clue.cs
+++ PFITS_Unity/Assets/Scripts/UI/Clue.cs
@@ -3,18 +3,41 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Clue : MonoBehaviour, IPointerDownHandler
 {
     public GameObject clueText;
     public GameObject clueNotebook;
+    public GameObject itemImage;
     public string description;
+    public bool pickUpItem;
+    private bool clueNoted;
+
+    private void Start()
+    {
+        //muss noch ins Saving eingebaut werden
+        if(pickUpItem && clueNoted)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        GameObject newClue = Instantiate(clueText, clueNotebook.transform);
-        newClue.GetComponent<TextMeshProUGUI>().text = description;
-        Destroy(gameObject);
+        if (!clueNoted)
+        {
+            GameObject newClue = Instantiate(clueText, clueNotebook.transform);
+            newClue.GetComponent<TextMeshProUGUI>().text = description;
+        }
+        if (pickUpItem)
+            Destroy(gameObject);
+        else
+        {
+            GameObject parentObject = itemImage.transform.parent.gameObject;
+            parentObject.SetActive(true);
+            itemImage.GetComponent<Image>().sprite = GetComponent<SpriteRenderer>().sprite;
+        }
+        clueNoted = true;
     }
-
 }
